c9ebd9b4d1ba05bcabc87d713774de38cb8f8cde
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-04-18 19:52:23 +0700

    Factorial

--- Arduino/FactorialFunction/FactorialFunction.ino
+++ Arduino/FactorialFunction/FactorialFunction.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <stdint.h>
+#include <itoa_ljust.h>
+#include <int96.h>
 
+int n, m;
+void setup() {
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  n = 1;
+  m = 100;
+
+  uint64_t fact = 1;
+  for(int x = n; x <= m; x++)
+  {
+    fact = fact * x;
+    Serial.print("The Factorial of ");
+    Serial.print(x);
+    Serial.print(" is ");
+    Serial.println(fact);
+    delay(100);
+  }
 
 }
---
+++ C++/FactorialFunction.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int factorial (int a, int n);
+{
+  int f;
+  int m;
+  m = 0
+  f=
+}
