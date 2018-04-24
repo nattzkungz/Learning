f4cec38bee728f7c529f47b196998ce8352d91ad
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-04-24 16:16:33 +0700

    Format

--- C++/Dividedby3.cpp
+++ C++/Dividedby3.cpp
@@ -3,13 +3,13 @@
 int x,y;
 
 int main(int argc, char const *argv[]) {
-  for (size_t x = 1; x < 16; x++) {
-    y=x%3;
-    if (y==0) {
-      std::cout << "Fizz" << '\n';
-    } else {
-      std::cout << x << '\n';
-    }
-  }
-  return 0;
+        for (size_t x = 1; x < 16; x++) {
+                y=x%3;
+                if (y==0) {
+                        std::cout << "Fizz" << '\n';
+                } else {
+                        std::cout << x << '\n';
+                }
+        }
+        return 0;
 }
--- C++/Start.cpp
+++ C++/Start.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-  std::cout << "Hello World!" << '\n';
-  std::cout << "Hi!\nYo!" << '\n';
-  int x;
-  x = 5;
-  std::cout << x << '\n';
-  return 0;
+        std::cout << "Hello World!" << '\n';
+        std::cout << "Hi!\nYo!" << '\n';
+        int x;
+        x = 5;
+        std::cout << x << '\n';
+        return 0;
 }
