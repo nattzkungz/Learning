5e9c419937dcb7482fa40db6eb906c9af3eb551d
Bose <bose@Boses-iMac.local>
2018-06-23 18:52:34 +0700

    For loop test

--- RasberryPI/ExampleCode/Servo.py
+++ RasberryPI/ExampleCode/Servo.py
@@ -4,6 +4,11 @@ import pigpio
 
 pi = pigpio.pi() # Connect to local Pi.
 
+for x in range(6):
+    print('x+1')
+    pass
+
+'''
 pi.set_servo_pulsewidth(4, 1000)
 time.sleep(2)
 pi.set_servo_pulsewidth(4, 1500)
@@ -15,5 +20,6 @@ time.sleep(2)
 
 # switch servo off
 pi.set_servo_pulsewidth(4, 0);
+'''
 
 pi.stop()
