8b36b1c07bebffcab49b8f10406b409fc894e133
Bose <bose@Boses-iMac.local>
2018-06-23 19:13:41 +0700

    Reupload

--- RasberryPI/ExampleCode/Servo.py
+++ RasberryPI/ExampleCode/Servo.py
@@ -5,22 +5,23 @@ import pigpio
 pi = pigpio.pi() # Connect to local Pi.
 
 pulse = None
+gpioServo = 4
 
 for x in range(20):
     pulse = (x * 100)+500   #turn  servo 100 pulse from 500-2500
     time.sleep(0.2)
-    pi.set_servo_pulsewidth(4, pulse)
+    pi.set_servo_pulsewidth(gpioServo, pulse)
     pass
 
 
 '''
-pi.set_servo_pulsewidth(4, 1000)
+pi.set_servo_pulsewidth(gpioServo, 1000)
 time.sleep(2)
-pi.set_servo_pulsewidth(4, 1500)
+pi.set_servo_pulsewidth(gpioServo, 1500)
 time.sleep(2)
-pi.set_servo_pulsewidth(4, 2000)
+pi.set_servo_pulsewidth(gpioServo, 2000)
 time.sleep(2)
-pi.set_servo_pulsewidth(4, 1500)
+pi.set_servo_pulsewidth(gpioServo, 1500)
 time.sleep(2)
 
 # switch servo off
