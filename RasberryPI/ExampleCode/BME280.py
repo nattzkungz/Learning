f141076176ddfc9d288935d12f30b105516177f2
Bose <bose@Boses-iMac.local>
2018-06-23 19:41:58 +0700

    Updated BME280.py.

--- RasberryPI/ExampleCode/BME280.py
+++ RasberryPI/ExampleCode/BME280.py
@@ -375,7 +375,7 @@ if __name__ == "__main__":
 
    while stop > time.time():
       t, p, h = s.read_data()
-      getAltitude = ((math.pow((sea_press / p), 1/5.257) - 1.0) * (t + 273.15)) / 0.0065; #Pressure to Altitude Equation
+      getAltitude = ((math.pow((sea_press / p/100.0), 1/5.257) - 1.0) * (t + 273.15)) / 0.0065; #Pressure to Altitude Equation
       print("h={:.2f} p={:.2f} t={:.2f} Alt={:.1f}".format(h, p/100.0, t, getAltitude)) #:.2f set decimal to 2 places
       time.sleep(0.9)
 
