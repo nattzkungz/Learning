83d57354f4e34945ff518c574bba6e1a63b81a2f
Bose <bose@Boses-iMac.local>
2018-06-24 14:17:47 +0700

    Updated StepMotorTest.py.

--- RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
+++ RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
@@ -9,16 +9,15 @@ delaystate = False
 currentTime = time.time()
 pi = pigpio.pi()
 motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence, delayAfterStep = 0)
-
-        for i in range(256):
-            if currentTime - previousTime > interval :
-            # save the last time the servo turn
-                previousTime = currentTime
-                if delaystate == False :
-                    delayState = True
-                    motor.doCounterclockwiseStep()
-                    motor.doCounterclockwiseStep()
-                    motor.doCounterclockwiseStep()
-                    motor.doCounterclockwiseStep()
-                else :
-                    delayState = False
+for i in range(256):
+    if currentTime - previousTime > interval :
+        # save the last time the servo turn
+        previousTime = currentTime
+            if delaystate == False :
+                delayState = True
+                motor.doCounterclockwiseStep()
+                motor.doCounterclockwiseStep()
+                motor.doCounterclockwiseStep()
+                motor.doCounterclockwiseStep()
+            else :
+                delayState = False
