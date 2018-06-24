664f315a44cb43eee5e8110f41c9b7b3997a5ca1
Bose <bose@Boses-iMac.local>
2018-06-24 11:45:48 +0700

    Updated StepMotorTest.py.

--- RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
+++ RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
@@ -4,4 +4,4 @@ from PigpioStepperMotor import StepperMotor
 pi = pigpio.pi()
 motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence)
 for i in range(1024):
-  motor.doClockwiseStep()
+  motor.doCounterclockwiseStepClockwiseStep()
