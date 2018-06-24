3806efb04348e2cce67fe3176f2e5c10824fde8c
Bose <bose@Boses-iMac.local>
2018-06-24 14:21:26 +0700

    Cannot do no delay!!

---
+++ RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTestWithNoDelay.py
@@ -0,0 +1,23 @@
+import pigpio
+from PigpioStepperMotor import StepperMotor, fullStepSequence
+import time
+
+#Variables
+previousTime = 0
+interval = 0.5
+delaystate = False
+currentTime = time.time()
+pi = pigpio.pi()
+motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence, delayAfterStep = 0)
+for i in range(256):
+    if currentTime - previousTime > interval :
+        # save the last time the servo turn
+        previousTime = currentTime
+        if delaystate == False :
+            delayState = True
+            motor.doCounterclockwiseStep()
+            motor.doCounterclockwiseStep()
+            motor.doCounterclockwiseStep()
+            motor.doCounterclockwiseStep()
+        else :
+            delayState = False
