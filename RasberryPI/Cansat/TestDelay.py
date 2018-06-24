6953849bb6107eae41c919d9a3d18e96461995f3
Bose <bose@Boses-iMac.local>
2018-06-24 14:46:27 +0700

    Updated TestDelay.py.

--- RasberryPI/Cansat/TestDelay.py
+++ RasberryPI/Cansat/TestDelay.py
@@ -28,11 +28,11 @@ if __name__ == '__main__':
         scheduler.print_jobs()
         print "-----------------"
 
-            print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-            try:
-        	       # This is here to simulate application activity (which keeps the main thread alive).
-                   while True:
-            	          time.sleep(2)
-    	    except (KeyboardInterrupt, SystemExit):
-        	       # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        	       scheduler.shutdown
+        print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+        try:
+            # This is here to simulate application activity (which keeps the main thread alive).
+            while True:
+                time.sleep(2)
+    	except (KeyboardInterrupt, SystemExit):
+        	# Not strictly necessary if daemonic mode is enabled but should be done if possible
+        	scheduler.shutdown
