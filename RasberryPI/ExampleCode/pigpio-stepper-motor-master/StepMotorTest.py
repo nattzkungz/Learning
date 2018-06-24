7eedf9ef09d6a685af4484ce0a0421e139080291
Bose <bose@Boses-iMac.local>
2018-06-24 13:58:44 +0700

    Indentation

--- RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
+++ RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
@@ -8,11 +8,12 @@ delaystate = False
 
 pi = pigpio.pi()
 motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence, delayAfterStep = 0)
-  if currentMillis - previousMillis > interval :
-    // save the last time the servo turn
-    previousMillis = currentMillis
+
+if currentMillis - previousMillis > interval :
+# save the last time the servo turn
+previousMillis = currentMillis
     if delaystate = False :
-      delayState = True
-      for i in range(512):
+    delayState = True
+        for i in range(512):
         motor.doCounterclockwiseStep()
         motor.doCounterclockwiseStep()
