8725e8a176a2a664ed97034d9b3fdf3576bf9d19
Bose <bose@Boses-iMac.local>
2018-06-24 13:56:03 +0700

    Step Motor with 2x full step

--- RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
+++ RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
@@ -1,7 +1,18 @@
 import pigpio
 from PigpioStepperMotor import StepperMotor, fullStepSequence
 
+#Variables
+previousMillis = 0
+interval = 0.5
+delaystate = False
+
 pi = pigpio.pi()
-motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence)
-for i in range(1024):
-  motor.doCounterclockwiseStep()
+motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence, delayAfterStep = 0)
+  if currentMillis - previousMillis > interval :
+    // save the last time the servo turn
+    previousMillis = currentMillis
+    if delaystate = False :
+      delayState = True
+      for i in range(512):
+        motor.doCounterclockwiseStep()
+        motor.doCounterclockwiseStep()
