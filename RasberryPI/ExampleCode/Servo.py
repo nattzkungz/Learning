91741c5be591ffc31ad07ea7a4f75e059d3a1458
Bose <bose@Boses-iMac.local>
2018-06-24 16:08:42 +0700

    Updated Servo.py.

--- RasberryPI/ExampleCode/Servo.py
+++ RasberryPI/ExampleCode/Servo.py
@@ -8,11 +8,11 @@ pulse = None
 gpioServo = 4
 servoPos = None
 
-for x in range(100):
-    if x == 40:
+for x in range(20):
+    if x == 20:
         break
     else :
-        pulse = (x * 20)+500   #turn  servo 100 pulse from 500-2500
+        pulse = (x * 50)+500   #turn  servo 100 pulse from 500-2500
         servoPos = x*9
         pi.set_servo_pulsewidth(gpioServo, pulse)
         print(servoPos)
