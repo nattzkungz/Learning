5c36da25bf88d735b9c0f03ce3fc523bf38c2da7
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-06-25 17:51:23 +0700

    Updated simpletest.py.

---
+++ RasberryPI/ExampleCode/StepperServo/StepMotorCounterclockwise.py
@@ -0,0 +1,8 @@
+import pigpio
+from PigpioStepperMotor import StepperMotor, fullStepSequence
+
+pi = pigpio.pi()
+motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence)
+for i in range(128):
+  motor.doCounterclockwiseStep()
+  motor.doCounterclockwiseStep()
