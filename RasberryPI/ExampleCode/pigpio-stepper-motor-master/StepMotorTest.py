90686583507384e196c1deced60e3646b8f2dfad
Bhumsitt Pramuanpornsatid <Bosebcc@Hotmail.com>
2018-06-25 17:11:03 +0700

    Reupload

--- RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
+++ RasberryPI/ExampleCode/pigpio-stepper-motor-master/StepMotorTest.py
@@ -1,10 +1,6 @@
 import pigpio
 from PigpioStepperMotor import StepperMotor, fullStepSequence
 
-#Variables
-previousMillis = 0
-interval = 0.5
-delaystate = false
 
 pi = pigpio.pi()
 motor = StepperMotor(pi, 6, 13, 19, 26, sequence = fullStepSequence)
