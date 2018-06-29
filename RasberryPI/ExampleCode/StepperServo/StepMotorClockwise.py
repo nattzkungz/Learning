1b0a52cbcf735ef7b5fdbb976c5c42ea8d67c80b
Bose <bose@iMac.local>
2018-06-29 17:30:08 +0700

    Updated StepMotorClockwise.py.

--- RasberryPI/ExampleCode/StepperServo/StepMotorClockwise.py
+++ RasberryPI/ExampleCode/StepperServo/StepMotorClockwise.py
@@ -1,10 +1,10 @@
-stepperWiringPi = require("../src/stepper-wiringpi");
-pinIN1 = 6;  // Stepper Red
-pinIN2 = 13;  // Stepper Blue
-pinIN3 = 19 ;// Stepper Green
-pinIN4 = 26; // Stepper Black
-motor = stepperWiringPi.setup(64, pinIN1, pinIN2, pinIN3, pinIN4);
+stepperWiringPi = require("../src/stepper-wiringpi")
+pinIN1 = 6
+pinIN2 = 13
+pinIN3 = 19
+pinIN4 = 26
+motor = stepperWiringPi.setup(64, pinIN1, pinIN2, pinIN3, pinIN4)
 
-motor.setSpeed(60);
-motor.step(64);
-motor.stop();
+motor.setSpeed(60)
+motor.step(64)
+motor.stop()
