742ba61e424687d11fe570f3c97878ad877ec7e7
Bose <bose@iMac.local>
2018-06-30 16:10:15 +0700

    Updated StepperServo.py.

--- RasberryPI/ExampleCode/StepperServo/StepperServo.py
+++ RasberryPI/ExampleCode/StepperServo/StepperServo.py
@@ -15,37 +15,35 @@ from PigpioStepperMotor import StepperMotor, halfStepSequence
 
 pi = pigpio.pi()
 motor = StepperMotor(pi, 6, 13, 19, 26, sequence = halfStepSequence)
-if __name__ == '__main__':
-    for i in range(2048):
-        motor.doCounterclockwiseStep()
-        for x in range(21):
-            pulse = (x * 100)+500   #turn  servo 100 pulse from 500-2500
-            pi.set_servo_pulsewidth(gpioServo, pulse)
-            time.sleep(0.025)
-            vis = sensor.readVisible()
-            IR = sensor.readIR()
-            UV = sensor.readUV()
-            uvIndex = UV / 100.0
-            if highVisible < uvIndex:
-                servoPos = x
-                stepPos = i
-                highVisible = uvIndex
-                print('SunLight Sensor read at time: %s' % datetime.now())
-                print '		Vis:             ' + str(vis)
-                print '		IR:              ' + str(IR)
-                print '		UV Index:        ' + str(uvIndex)
+for i in range(2048):
+  motor.doCounterclockwiseStep()
+  for x in range(21):
+    pulse = (x * 100)+500   #turn  servo 100 pulse from 500-2500
+    pi.set_servo_pulsewidth(gpioServo, pulse)
+    time.sleep(0.025)
+    vis = sensor.readVisible()
+    IR = sensor.readIR()
+    UV = sensor.readUV()
+    uvIndex = UV / 100.0
+    if highVisible < uvIndex:
+        servoPos = x
+        stepPos = i
+        highVisible = uvIndex
+    print('SunLight Sensor read at time: %s' % datetime.now())
+    print '		Vis:             ' + str(vis)
+    print '		IR:              ' + str(IR)
+    print '		UV Index:        ' + str(uvIndex)
 
-                if uvIndex <= 3 :
-                    print "Warning:" + "Wear Sun Glass; Low UV"
-                elif uvIndex > 3 and uvIndex <= 6 :
-                    print "Warning:" + "Take cover when avalible; Moderate UV"
-                elif uvIndex > 6 and uvIndex >= 8 :
-                    print "Warning:" + "Apply SPF 30+ sunscreen, don't stay out more than 3 hours; High UV"
-                elif uvIndex > 8 and uvIndex >= 11 :
-                    print "Warning:" + "Do not stay in the sun for too long; Very High UV"
-                else :
-                    print "Warning:" + "Take all Percautions; Extreme UV"
-pi.stop()
+    if uvIndex <= 3 :
+        print "Warning:" + "Wear Sun Glass; Low UV"
+    elif uvIndex > 3 and uvIndex <= 6 :
+        print "Warning:" + "Take cover when avalible; Moderate UV"
+    elif uvIndex > 6 and uvIndex >= 8 :
+        print "Warning:" + "Apply SPF 30+ sunscreen, don't stay out more than 3 hours; High UV"
+    elif uvIndex > 8 and uvIndex >= 11 :
+        print "Warning:" + "Do not stay in the sun for too long; Very High UV"
+    else :
+        print "Warning:" + "Take all Percautions; Extreme UV"
 '''
 if __name__ == '__main__':
     servoPos = (servoPos * 100)+500
