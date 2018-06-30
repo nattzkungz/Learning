f27eaf999399d0ad3ea069c83096d1826750aab1
Bose <bose@iMac.local>
2018-06-30 16:18:00 +0700

    Updated StepperServo.py.

--- RasberryPI/ExampleCode/StepperServo/StepperServo.py
+++ RasberryPI/ExampleCode/StepperServo/StepperServo.py
@@ -44,6 +44,30 @@ for i in range(2048):
         print "Warning:" + "Do not stay in the sun for too long; Very High UV"
     else :
         print "Warning:" + "Take all Percautions; Extreme UV"
+
+#clear GPIO
+counter = 0
+
+try:
+    # here you put your main loop or block of code
+    while counter < 9000000:
+        # count up to 9000000 - takes ~20s
+        counter += 1
+    print "Target reached: %d" % counter
+
+except KeyboardInterrupt:
+    # here you put any code you want to run before the program
+    # exits when you press CTRL+C
+    print "\n", counter # print value of counter
+
+except:
+    # this catches ALL other exceptions including errors.
+    # You won't get any error messages for debugging
+    # so only use it once your code is working
+    print "Other error or exception occurred!"
+
+finally:
+    GPIO.cleanup() # this ensures a clean exit
 '''
 if __name__ == '__main__':
     servoPos = (servoPos * 100)+500
